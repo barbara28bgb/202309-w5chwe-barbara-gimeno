2803533da9c100d425aa6344e174e3270ffc6e89
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-10-14 19:20:57 +0200

    Create classes better

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 import Cell from "./Cell/Cell.js";
+
 import World from "./world/world.js";
 
 const board = new World();
 
-const cell = new Cell(true);
 board.initBoard(4, true);
 
-console.log(initBoard);
+console.log(board.initBoard(4, true));
